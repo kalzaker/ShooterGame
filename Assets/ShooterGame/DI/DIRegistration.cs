449fcee50732e1d5d24db0db98844e0798e29a00
kalzaker <misha311959@gmail.com>
2024-11-14 20:18:34 +0100

    DI Container

---
+++ Assets/ShooterGame/DI/DIRegistration.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DI
+{
+    public class DIRegistration
+    {
+        public Func<DIContainer, object> Factory { get; set; }
+        public bool IsSingleton { get; set; }
+        public object Instance { get; set; }
+    }
+}
